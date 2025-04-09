01252bfc8ef0f7616587bdb143a630bd47400bd0
Migs <138418306+Wrenas0@users.noreply.github.com>
2025-04-09 08:15:46 -0300

    Drag & Drop (Corrigido)

--- Drag Drop/Projeto/script.js
+++ Drag Drop/Projeto/script.js
@@ -1,17 +1,24 @@
-const tarefas = document.querySelectorAll(".tarefa")
-const colunas = document.querySelectorAll(".tarefas")
+const tarefas = document.querySelectorAll(".tarefa");
+const colunas = document.querySelectorAll(".tarefas");
 
-tarefas.forEach(tarefa =>{
-    tarefa.addEventListener("dragstart", ()=>{
-        tarefa.classList.add("arrastando")
-    })
-    tarefa.addEventListener.remove("arrastando")
-})
+tarefas.forEach(tarefa => {
+    tarefa.addEventListener("dragstart", () => {
+        tarefa.classList.add("arrastando");
+    });
+    
+    tarefa.addEventListener("dragend", () => {
+        tarefa.classList.remove("arrastando");
+    });
+});
 
 colunas.forEach(coluna => {
-    coluna.addEventListener("dragover", (e) =>{
-        e.preventDefault()
-        const tarefaArrastando = document.querySelector("arrastando")
-        coluna.appendChild(tarefaArrastando)
-    })
-})
+    coluna.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const tarefaArrastando = document.querySelector(".arrastando");
+        if (tarefaArrastando) {
+            if (!coluna.contains(tarefaArrastando)) {
+                coluna.appendChild(tarefaArrastando);
+            }
+        }
+    });
+});
